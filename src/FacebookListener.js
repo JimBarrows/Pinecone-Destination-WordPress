3ee150d98b71a6c708b5aad24c7066de97db28db
Jim Barrows <jim@reallybigtree.com>
2016-07-23 11:14:18 -0700

    Adding pinecone models to destiantions

---
+++ src/FacebookListener.js
@@ -0,0 +1,10 @@
+/**
+ * Created by JimBarrows on 7/19/16.
+ */
+'use strict';
+
+import {FB, FacebookApiException} from "facebook-node-sdk";
+
+const appId = '1236802509686356';
+const appSecret = '80fd99d15c54490d6558e3883c1bc40f';
+const version = "v2.7";
